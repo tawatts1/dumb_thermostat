624b43bbaf8d8abd1ab7e63cabba64e49d0a6ce3
Ted Watts <tedawatts@gmail.com>
2021-06-27 13:59:24 -0700

    file to take data

---
+++ write_data.py
@@ -0,0 +1,28 @@
+import bme280
+import smbus2
+from time import sleep
+import RPi.GPIO as GPIO
+
+port = 1
+address = 0x77 # Adafruit BME280 address. Other BME280s may be different
+bus = smbus2.SMBus(port)
+
+bme280.load_calibration_params(bus,address)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+
+fname = "data0.txt"
+
+while True:
+    bme280_data = bme280.sample(bus,address)
+    humidity  = bme280_data.humidity
+    pressure  = bme280_data.pressure
+    ambient_temperature = bme280_data.temperature
+    fafhrenheite = 9.0/5.0 * ambient_temperature + 32
+    string = f'{humidity},\t{fafhrenheite},\t{pressure}'
+    print(string + '             ', end = '\r')
+    with open(fname, 'a') as file:
+        file.write(string)
+    sleep(1)
+
