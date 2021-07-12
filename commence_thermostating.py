ca45d992b114eac06509d706a5a26cad32e0520b
John <jchurch21@students.claremontmckenna.edu>
2021-07-11 22:55:33 -0700

    beautify

--- commence_thermostating.py
+++ commence_thermostating.py
@@ -6,43 +6,46 @@ import thermostat
 
 
 port = 1
-address = 0x77 # Adafruit BME280 address. Other BME280s may be different
+address = 0x77  # Adafruit BME280 address. Other BME280s may be different
 bus = smbus2.SMBus(port)
 
-bme280.load_calibration_params(bus,address)
+bme280.load_calibration_params(bus, address)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 
 def led_condition(f):
-    if round((f%.1)*1111000)%7 == 0:
+    if round((f % .1) * 1111000) % 7 == 0:
         return True
     return False
 
+
 while True:
-    bme280_data = bme280.sample(bus,address)
-    humidity  = bme280_data.humidity
-    pressure  = bme280_data.pressure
+    bme280_data = bme280.sample(bus, address)
+    humidity = bme280_data.humidity
+    pressure = bme280_data.pressure
     ambient_temperature = bme280_data.temperature
-    fafhrenheite = 9.0/5.0 * ambient_temperature + 32
-    print('Humidity: {0}, Temperature: {1}, Pressure: {2}'.format(humidity, fafhrenheite, pressure))
-    GPIO.setup(18,GPIO.OUT)
+    fafhrenheite = 9.0 / 5.0 * ambient_temperature + 32
+    print('Humidity: {0}, Temperature: {1}, Pressure: {2}'.format(
+        humidity, fafhrenheite, pressure))
+    GPIO.setup(18, GPIO.OUT)
     print("LED on")
-    GPIO.output(18,GPIO.HIGH)
+    GPIO.output(18, GPIO.HIGH)
     sleep(1)
     print("LED off")
-    GPIO.output(18,GPIO.LOW)
+    GPIO.output(18, GPIO.LOW)
 
     mode = 'cool'
-    current_temp_f = fafhrenheite #
-    current_state = 'off'# on/off
-    minutes_in_state = 0 #
+    current_temp_f = fafhrenheite
+    current_state = 'off'  # on/off
+    minutes_in_state = 0
 
     therm = thermostat.thermostat("thermostat.config", mode)
     code = therm.l2_switching(current_temp_f, current_state, minutes_in_state)
     print(code)
     with open('logs.txt', 'a') as file:
-        file.write('{4}  Humidity: {0}, Temperature: {1}, Pressure: {2}, Code: {3}'.format(humidity, fafhrenheite, pressure, code, therm.time_now()))
+        file.write('{4}  Humidity: {0}, Temperature: {1}, Pressure: {2}, Code: {3}'.format(
+            humidity, fafhrenheite, pressure, code, therm.time_now()))
         file.write('\n')
     sleep(4)
--- thermostat.py
+++ thermostat.py
@@ -9,14 +9,16 @@ from ast import literal_eval
 from datetime import datetime
 import sys
 
+
 def write_and_print(error, error_file):
     with open(error_file, 'a') as file:
-        file.write('------\n' + \
-                    str(datetime.now()) +      \
-                    ':\n' +                    \
-                    str(error) +               \
-                    '\n')
+        file.write('------\n' +
+                   str(datetime.now()) +
+                   ':\n' +
+                   str(error) +
+                   '\n')
     print(error)
+
 
 def in_intervals(val, intervals):
     '''
@@ -24,15 +26,16 @@ def in_intervals(val, intervals):
     '''
     for interval in intervals:
         if (val < interval[1] and val >= interval[0]) or \
-        (interval[1]<interval[0] and (val < interval[1] or val >= interval[0])):
+                (interval[1] < interval[0] and (val < interval[1] or val >= interval[0])):
             return True
     return False
 
+
 class thermostat():
-    def __init__(self, config_file, current_mode, error_file = "errors.txt"):
+    def __init__(self, config_file, current_mode, error_file="errors.txt"):
         self.error_file = error_file
         self.mode = current_mode
-        #try:
+        # try:
         with open(config_file, 'r') as file:
             txt = file.read()
         settings = literal_eval(txt)
@@ -42,29 +45,33 @@ class thermostat():
                 time_str = self.float_to_time(key)
                 settings[mode][time_str] = settings[mode].pop(key)
         self.settings = settings
-        #except Exception as error:
+        # except Exception as error:
         #   write_and_print(error, error_file)
+
     def time_now(self):
         return datetime.now().strftime("%H:%M")
+
     def get_target_temp(self):
         now = self.time_now()
         dic = self.settings[self.mode]
         times = list(dic.keys())
         times.sort()
-        #print(times)
+        # print(times)
         #L = len(times)
-        for i in range(len(times)-1, -1, -1):
+        for i in range(len(times) - 1, -1, -1):
             if now > times[i]:
                 thermostat_time = times[i]
                 break
         else:
             thermostat_time = times[0]
         return self.settings[self.mode][thermostat_time]
+
     def float_to_time(self, flt):
         div, mod = divmod(float(flt), 1)
-        div, mod = int(div), int(mod*60)
+        div, mod = int(div), int(mod * 60)
         time_str = '{:02d}:{:02d}'.format(div, mod)
         return time_str
+
     def l1_switching(self, current_temp_f):
         '''
         l1 can be overridden by l2 and l3
@@ -80,8 +87,8 @@ class thermostat():
         '''
 
         target_temp = self.get_target_temp()
-        sign = {"cool":1,"heat":-1}[self.mode]
-        diff = sign*(current_temp_f-target_temp)
+        sign = {"cool": 1, "heat": -1}[self.mode]
+        diff = sign * (current_temp_f - target_temp)
 
         if diff > self.settings["threshhold"]:
             out = 1
@@ -91,54 +98,41 @@ class thermostat():
             out = 0
         return out
 
-
     def l2_switching(self, current_temp_f, state, minutes_in_state):
         # if it has run to long, turn it off no matter what
         # if it has only been off for a short time minutes, keep it off
         if state == 'on':
             if minutes_in_state > self.settings["max_on"]:
-                return -1 # make it rest
+                return -1  # make it rest
             elif minutes_in_state < self.settings["min_on"]:
-                return 0 # keep it on
-        elif state=='off' and minutes_in_state < self.settings["min_off"]:
-            return 0 # keep it off so it can rest
+                return 0  # keep it on
+        elif state == 'off' and minutes_in_state < self.settings["min_off"]:
+            return 0  # keep it off so it can rest
 
         out = self.l2_switching(current_temp_f)
         if out == 1 and state == 'on':
-            out = 0 # No need to send a 1
+            out = 0  # No need to send a 1
         elif out == -1 and state == 'off':
             out = 0
         return out
 
 
-
-
-
-
-
-
-
-
-
 def test1():
     x = thermostat("thermostat.config", "cool")
-    for t in [75,75.5,7,76.5,77,77.5,78,78.5,79,79.5,80,80.5]:
+    for t in [75, 75.5, 7, 76.5, 77, 77.5, 78, 78.5, 79, 79.5, 80, 80.5]:
         print(t, x.l1_switching(t))
+
 
 if __name__ == "__main__":
     mode = sys.argv[1]
-    current_temp_f = sys.argv[2] #
-    current_state = sys.argv[3]# on/off
-    minutes_in_state = sys.argv[4] #
+    current_temp_f = sys.argv[2]
+    current_state = sys.argv[3]  # on/off
+    minutes_in_state = sys.argv[4]
 
     therm = thermostat("thermostat.config", mode)
     print(therm.l2_switching(current_temp_f, current_state, minutes_in_state))
 
 
-
-
-
-
 x = thermostat("thermostat.config", "cool")
-for t in [75,75.5,7,76.5,77,77.5,78,78.5,79,79.5,80,80.5]:
+for t in [75, 75.5, 7, 76.5, 77, 77.5, 78, 78.5, 79, 79.5, 80, 80.5]:
     print(t, x.l1_switching(t))
