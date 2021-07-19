8061a5b2d57bf97f1ff0671798be7892e1712a48
Ted Watts <tedawatts@gmail.com>
2021-07-18 20:07:23 -0700

    altered random_blink for testing

--- random_blink.py
+++ random_blink.py
@@ -2,12 +2,13 @@ import bme280
 import smbus2
 from time import sleep
 import RPi.GPIO as GPIO
-
+'''
 port = 1
 address = 0x77 # Adafruit BME280 address. Other BME280s may be different
 bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus,address)
+'''
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
@@ -16,20 +17,23 @@ def led_condition(f):
     if round((f%.1)*1111000)%7 == 0:
         return True
     return False
-
-while True:
+i=0
+for i in range(100):
+    '''
     bme280_data = bme280.sample(bus,address)
     humidity  = bme280_data.humidity
     pressure  = bme280_data.pressure
     ambient_temperature = bme280_data.temperature
     fafhrenheite = 9.0/5.0 * ambient_temperature + 32
     print('Humidity: {0}, Temperature: {1}, Pressure: {2}'.format(humidity, fafhrenheite, pressure))
-    if led_condition(fafhrenheite):
+    '''
+    if i%5==1:
         GPIO.setup(18,GPIO.OUT)
         print("LED on")
         GPIO.output(18,GPIO.HIGH)
     else:
         print("LED off")
         GPIO.output(18,GPIO.LOW)
-    sleep(1)
+    sleep(.4)
+    i += 1
 
