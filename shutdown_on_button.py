d5472131ce5cc1d5288ac95c628dedafad664118
Ted Watts <tedawatts@gmail.com>
2021-08-28 14:06:34 -0700

    Added output file to record startup and shutdown

--- shutdown_on_button.py
+++ shutdown_on_button.py
@@ -1,6 +1,11 @@
 from time import sleep
 from subprocess import call
+from datetime import datetime
 import gpio_utils
+
+fname = "power_record.txt"
+with open(fname, 'a') as file:
+    file.write(datetime.now().strftime('%y-%m-%d %H:%M:%S') + ',1')
 
 gpio_utils.set_mode()
 N=26
@@ -9,6 +14,8 @@ while True:
     sleep(.5)
     if gpio_utils.gpio_get(N) == 1:
         #print('button pressed')
+        with open(fname, 'a') as file: 
+            file.write(datetime.now().strftime('%y-%m-%d %H:%M:%S') + ',1')
         call('sudo shutdown -h now'.split(' '))
         break
     else:
